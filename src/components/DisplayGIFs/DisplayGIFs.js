3634f762cbd2f5f5e39fe7fc0723dba941b25bab
Karl Beck <karljohnbeck@gmail.com>
2020-08-27 14:01:53 -0500

    DO NOT MERGE THIS

--- src/components/DisplayGIFs/DisplayGIFs.js
+++ src/components/DisplayGIFs/DisplayGIFs.js
@@ -1,18 +1,21 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import axios from 'axios';
 
 function Display(props) {
     saveGIF = (gif) => {
-        this.props.dispatch({type: 'SAVE_FAV', payload: gif})
+        this.props.dispatch({ type: 'SAVE_FAV', payload: gif })
     }
+
     return (
-        {this.props.reduxState.searchResults.map((gif, i) => (
-            <div key={i}>
-                <img src={gif.url} alt='gif' />
-                <button onClick={()=>this.saveGIF(gif)}>Favorite</button>
-            </div>
-        ))}
+        {
+            this.props.reduxState.searchResults.map((gif, i) => (
+                <div key={i}>
+                    <img src={gif.url} alt='gif' />
+                    <button onClick={() => this.saveGIF(gif)}>Favorite</button>
+                </div>
+            ))
+        }
     )
 };
 
