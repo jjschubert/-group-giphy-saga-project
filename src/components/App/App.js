f20919a5bbb29cdb7b45e673abac463db4e80b68
Karl Beck <karljohnbeck@gmail.com>
2020-08-27 13:11:52 -0500

    added routers to git

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react';
 import Search from '../SearchGIF/SearchGIF.js'
+import { HashRouter as Router, Route, Link } from 'react-router-dom'
+import SearchGIF from '../SearchGIF/SearchGIF.js';
 
 
 class App extends Component {
 
   render() {
     return (
-      <div>
-        <h1>Giphy Search!</h1>
-        <Search />
-      </div>
+      <Router>
+        <header>
+        <div>
+          <h1>Giphy Search!</h1>
+        </div>
+        <nav>
+          <Link to="/">Search</Link>
+          <Link to="/favorites" >Favorites</Link>
+        </nav>
+        </header>
+        
 
+        <Route exact path="/" component={SearchGIF} />
+        <Route path="/favorites" component={Favorites} />
+
+      </Router>
     );
   }
-  
+
 }
 
 export default App;
