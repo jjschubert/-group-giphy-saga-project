bfe4da0c40d3daddc498ee801bfa670915ffdece
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-27 11:45:00 -0500

    basic display

--- src/components/Favorites/Favorites.js
+++ src/components/Favorites/Favorites.js
@@ -1,19 +1,31 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux'
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class Favorite extends Component {
-  
-componentDidMount() {
-    this.props.dispatch({type: 'FETCH_FAVORITES'})
-}
+
+    componentDidMount() {
+        this.props.dispatch({ type: 'FETCH_FAVORITES' })
+    }
 
     render() {
-        return(
+        return (
             <>
-            <h3>Favorites</h3>
+                <h3>Favorites</h3>
+                {this.props.reduxState.favoriteGifs.map((gif) => {
+                    return (
+                        <>
+                            <img src={gif.image_path} alt={gif.name} key={gif.id}/>
+                            <p>{gif.name}</p>
+                        </>
+                    )
+                })}
             </>
         )
     }
 }
 
-export default connect()(Favorite)
+const mapStateToProps = reduxState => ({
+    reduxState,
+});
+
+export default connect(mapStateToProps)(Favorite)
