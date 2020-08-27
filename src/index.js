fc0dc8f2c19326b7c281e12fd9329d128cd3f824
Karl Beck <karljohnbeck@gmail.com>
2020-08-27 11:12:12 -0500

    created the routes for the favorite table in server side GET PUT POST

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import logger from 'redux-logger'
+import axios from 'axios'
+import createSagaMiddleware from 'redux-saga'
+import {takeEvery, put} from 'redux-saga/effects'
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+
+
+
+ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('react-root'));
