f50f6f83aa8296573b96a130c3fd20dad9f18bb2
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-28 14:25:43 -0500

    key fix

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -5,7 +5,7 @@ const axios = require('axios');
 
 router.get('/:query', (req, res) => {
     console.log(req.params.query);
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.query}&api_key=eAM6qlh7XtczlCVEqDHqxVIS8FPAfL76`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.query}&api_key=${GIPHY_KEY}`)
     .then(response => {
         res.send(response.data)
     }).catch(error => {
