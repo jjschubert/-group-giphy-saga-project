9c95a7578b21740c1a488bf6754cdd4aa5650337
Jenni Schubert <jenni.schubert@gmail.com>
2020-08-28 14:28:51 -0500

    key fix

--- server/routes/search.router.js
+++ server/routes/search.router.js
@@ -2,10 +2,11 @@ const express = require('express');
 const pool = require('../modules/pool');
 const router = express.Router();
 const axios = require('axios');
+require('dotenv').config();
 
 router.get('/:query', (req, res) => {
     console.log(req.params.query);
-    axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.query}&api_key=${GIPHY_KEY}`)
+    axios.get(`http://api.giphy.com/v1/gifs/search?q=${req.params.query}&api_key=${process.env.GIPHY_KEY}`)
     .then(response => {
         res.send(response.data)
     }).catch(error => {
